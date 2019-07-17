f03215d5286f17bf85e768ca913faacdc0f8c9a0
garza4 <garza4@southwestern.edu>
2019-07-17 12:03:30 -0500

    using calculator example

--- bowlingServer/main.go
+++ bowlingServer/main.go
@@ -1,44 +1,58 @@
 package main
-import "fmt"
+import( 
+	//"encoding/json"
+	//"flag"
+	"log"
+	//"io/ioutil"
+	"fmt"
+	"context"
+	//"math"
+	"net"
+	//"sync"
+	//"time"
+
+	"google.golang.org/grpc"
+	//"github.com/golang/protobuf/proto"
+	pb "golangBowlingAPI/golangBowlingAPI"
 
 
-type bowlingAPIServer struct{
+)
+
+//var port = flag.Int("port",10000, "The Server Port")
+
+
+
+type BowlingServiceServer struct{}
+
+
+
+
+
+//throw the bowling ball and get the result from the throw...
+//pb is the generated protoc file. 
+func (s *BowlingServiceServer) Bowl(ctx context.Context, throw *pb.Throw) (*pb.Score,error){
+	score := *pb.GetScore()
+
+	return &score,nil
 
 }
 
-type Game struct{
-	numPlayers int
-	playerScore []int
-	playerTurn []bool
 
-}
-
-func startGame(g Game){
-	numPlayers := getNumPlayers(g)
-	for i, num := range g.playerScore{
-		g.playerScore[i] = 0
-	}
-
-}
-
-
-//throw the bowling ball
-func (s *bowlingAPIServer) Throw(ctx context.Context, throw *pb.Throw) (pb.Feature,error){
-
-
-}
-
-
-//get the score after a throw
-func (s *bowlingAPIServer) Score(ctx context.Context, score *pb.Score) (pb.Feature,error){
-
-
-}
 
 
 
 
 
 func main(){
+	address := "localhost:50051"
+	lis,err := net.Listen("tcp",address)
+	if err != nil {
+		log.Fatalln("Failed to Listen: %v", err)
+	}
+	bServer := &BowlingServiceServer{}
+	grpcServer := grpc.NewServer()
+	pb.RegisterBowlingServiceServer(grpcServer,bServer)
+
+
 	fmt.Printf("Hellcooper \n")
 }
