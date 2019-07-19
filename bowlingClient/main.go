28177841a513ee8696110778ca3053a701fbf2f4
garza4 <garza4@southwestern.edu>
2019-07-18 21:12:13 -0500

    working on client side, need to learn to go bowling

--- bowlingClient/main.go
+++ bowlingClient/main.go
@@ -1,8 +1,18 @@
 package main
-import fmt
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	pb "golangBowlingAPI/golangBowlingAPI"
+)
 
 func main(){
-	conn,err := credentials
+	conn,err := grpc.Dial("localhost:50051")
+	if err != nil{
+	
+	}
+	defer conn.Close()
+	client := pb.NewBowlingAPIClient(conn)
+	play,err := client.Bowl(context.Background(),&pb.Throw()) //play bowling? 
 	fmt.printF("Hello Cooper")
 
 
